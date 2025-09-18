72e252383884ea140be8041ea2f765d21fe53a77
Roberts Slisans <rsxdalv@gmail.com>
2025-09-18 13:32:13 +0300

    migrate to pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 import setuptools
+from pathlib import Path
+
+HERE = Path(__file__).parent
+README = (HERE / "README.md").read_text(encoding="utf-8") if (HERE / "README.md").exists() else ""
 
 setuptools.setup(
     name="tts_webui_extension.seamless_m4t",
     packages=setuptools.find_namespace_packages(),
-    version="0.0.4",
+    version="0.0.5",
     author="rsxdalv",
     description="SeamlessM4T is a multilingual and multimodal translation model supporting text and speech",
+    long_description=README,
+    long_description_content_type="text/markdown",
     url="https://github.com/rsxdalv/tts_webui_extension.seamless_m4t",
     project_urls={},
     scripts=[],
